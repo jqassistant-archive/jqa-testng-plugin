0a3de3a5c2c06cb13dfb31f8de85161e9d01e534
Dirk Mahler <dirk.mahler@buschmais.com>
2019-11-26 11:06:31 +0100

    moved report api model to package core.report.model

--- src/test/java/com/buschmais/jqassistant/plugin/testng/test/TestNGIT.java
+++ src/test/java/com/buschmais/jqassistant/plugin/testng/test/TestNGIT.java
@@ -8,7 +8,7 @@ import com.buschmais.jqassistant.plugin.testng.test.set.test.TestClass;
 
 import org.junit.jupiter.api.Test;
 
-import static com.buschmais.jqassistant.core.analysis.api.Result.Status.SUCCESS;
+import static com.buschmais.jqassistant.core.report.api.model.Result.Status.SUCCESS;
 import static com.buschmais.jqassistant.plugin.java.test.matcher.MethodDescriptorMatcher.methodDescriptor;
 import static com.buschmais.jqassistant.plugin.java.test.matcher.TypeDescriptorMatcher.typeDescriptor;
 import static org.hamcrest.CoreMatchers.equalTo;
