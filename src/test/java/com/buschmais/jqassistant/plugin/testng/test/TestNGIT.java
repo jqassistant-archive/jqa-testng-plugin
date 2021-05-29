bbc465e5c676f3c32e982ba11ed8fa0f282fbb09
Dirk Mahler <dirk.mahler@buschmais.com>
2021-05-30 00:05:52 +0200

    replaced deprecated org.junit.Assert.assertThat with org.hamcrest.MatcherAssert.assertThat

--- src/test/java/com/buschmais/jqassistant/plugin/testng/test/TestNGIT.java
+++ src/test/java/com/buschmais/jqassistant/plugin/testng/test/TestNGIT.java
@@ -13,7 +13,7 @@ import static com.buschmais.jqassistant.plugin.java.test.matcher.MethodDescripto
 import static com.buschmais.jqassistant.plugin.java.test.matcher.TypeDescriptorMatcher.typeDescriptor;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.hasItem;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 /**
  * Tests for TestNG concepts.
